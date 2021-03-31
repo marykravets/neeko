6485a34b12a4f67a8f7816bec1b42b3c94a4cecb
Mariana <mary.kravets@gmail.com>
2021-03-31 10:48:24 -0400

    migration, null safety, cleanup

--- example/android/app/src/main/java/com/jarvanmo/neeko_sample/MainActivity.java
+++ example/android/app/src/main/java/com/jarvanmo/neeko_sample/MainActivity.java
@@ -2,16 +2,22 @@ package com.jarvanmo.neeko_sample;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import androidx.annotation.NonNull;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
 
+    private static final String CHANNEL = "samples.flutter.dev/battery";
+
     @Override
-   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
-        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+        new MethodChannel(
+                flutterEngine.getDartExecutor().getBinaryMessenger(),
+                CHANNEL)
                 .setMethodCallHandler(
                         (call, result) -> {
-                            // Your existing code
                         }
                 );
     }
